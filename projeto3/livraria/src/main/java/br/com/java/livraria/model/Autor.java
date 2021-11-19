f64c315a581247421ec80ff17c097aefed4ec3af
Ricardo <rcbull@gmail.com>
2021-11-19 15:48:51 -0300

    corrigindo merge do projeto 3

--- projeto3/livraria/src/main/java/br/com/java/livraria/model/Autor.java
+++ projeto3/livraria/src/main/java/br/com/java/livraria/model/Autor.java
@@ -2,15 +2,29 @@ package br.com.java.livraria.model;
 
 import java.time.LocalDate;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
 @Getter
 @Setter
 @ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "autores")
 public class Autor {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nome;
 	private String email;
--- projeto3/livraria/src/main/java/br/com/java/livraria/service/AutorService.java
+++ projeto3/livraria/src/main/java/br/com/java/livraria/service/AutorService.java
@@ -4,30 +4,35 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.transaction.Transactional;
+
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 
 import br.com.java.livraria.dto.AutorDto;
 import br.com.java.livraria.dto.AutorFormDto;
 import br.com.java.livraria.model.Autor;
+import br.com.java.livraria.repository.AutorRepository;
 
 @Service
 public class AutorService {
 
-	private List<Autor> autores = new ArrayList<>();
+	@Autowired
+	private AutorRepository autorRepository;
 	private ModelMapper modelMapper = new ModelMapper();
 
-	public List<AutorDto> listar() {
-
-		return autores.stream().map(t -> modelMapper.map(t, AutorDto.class)).collect(Collectors.toList());
+	public Page<AutorDto> listar(Pageable paginacao) {
+		Page<Autor> autores = autorRepository.findAll(paginacao);
+		return autores.map(t -> modelMapper.map(t, AutorDto.class));
 	}
 
+	@Transactional
 	public void cadastrar(AutorFormDto dto) {
-
 		Autor autor = new ModelMapper().map(dto, Autor.class);
-
-		autores.add(autor);
-
+		autor.setId(null);
+		autorRepository.save(autor);
 	}
-
 }
