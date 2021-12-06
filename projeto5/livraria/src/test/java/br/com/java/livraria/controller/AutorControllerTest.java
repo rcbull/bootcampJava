7735cb423d79214b2927008a7bb2c321bdb37000
Ricardo <rcbull@gmail.com>
2021-12-06 11:11:57 -0300

    refatorando os testes do projeto 5

--- projeto5/livraria/src/test/java/br/com/java/livraria/controller/AutorControllerTest.java
+++ projeto5/livraria/src/test/java/br/com/java/livraria/controller/AutorControllerTest.java
@@ -7,15 +7,25 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+
+import br.com.java.livraria.infra.security.TokenService;
+import br.com.java.livraria.model.Perfil;
+import br.com.java.livraria.model.Usuario;
+import br.com.java.livraria.repository.PerfilRepository;
+import br.com.java.livraria.repository.UsuarioRepository;
 
 
 @ExtendWith(SpringExtension.class)
@@ -28,11 +38,36 @@ public class AutorControllerTest {
 	@Autowired
 	private MockMvc mvc;
 	
+	@Autowired
+	private TokenService tokenService;
+	
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+	
+	@Autowired
+	private PerfilRepository perfilRepository;
+	
+	@Autowired
+	private ModelMapper modelMapper;
+	
+	private String token;
+	
+	@BeforeEach
+	public void gerarToken() {
+		Usuario logado = new Usuario("admin", "admin", "admin");
+		Perfil admin = perfilRepository.findById(1l).get();
+		logado.adicionarPerfil(admin);
+		usuarioRepository.save(logado);
+		Authentication authentication = new UsernamePasswordAuthenticationToken(logado, logado.getLogin());
+		this.token = tokenService.gerarToken(authentication);		
+	}
+	
 	@Test
 	public void naoDeveriaCadastrarAutorComDadosIncompletos() throws Exception {
 		String json = "{}";
 
-		mvc.perform(post("/autores").contentType(MediaType.APPLICATION_JSON).content(json)).andExpect(status().isBadRequest());
+		mvc.perform(post("/autores").contentType(MediaType.APPLICATION_JSON).content(json).header("Authorization",
+				"Bearer " + token)).andExpect(status().isBadRequest());
 	}
 
 	@Test
@@ -42,7 +77,8 @@ public class AutorControllerTest {
 		String jsonRetornado = "{\"nome\":\"Robert C. Martin\"," + "\"email\":\"unclebob@gmail.com\","
 				+ "\"dataNascimento\":\"1952-12-5\"," + "\"miniCurriculo\":\"clean code...\"}";
 
-		mvc.perform(post("/autores").contentType(MediaType.APPLICATION_JSON).content(json)).andExpect(status().isCreated()).andExpect(header().exists("Location"))
+		mvc.perform(post("/autores").contentType(MediaType.APPLICATION_JSON).content(json).header("Authorization",
+				"Bearer " + token)).andExpect(status().isCreated()).andExpect(header().exists("Location"))
 				.andExpect(content().json(jsonRetornado));
 
 	}
--- projeto5/livraria/src/test/java/br/com/java/livraria/controller/LivroControllerTest.java
+++ projeto5/livraria/src/test/java/br/com/java/livraria/controller/LivroControllerTest.java
@@ -5,12 +5,15 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
@@ -18,6 +21,12 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.jayway.jsonpath.JsonPath;
+
+import br.com.java.livraria.infra.security.TokenService;
+import br.com.java.livraria.model.Perfil;
+import br.com.java.livraria.model.Usuario;
+import br.com.java.livraria.repository.PerfilRepository;
+import br.com.java.livraria.repository.UsuarioRepository;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
@@ -29,11 +38,33 @@ public class LivroControllerTest {
 	@Autowired
 	private MockMvc mvc;
 
+	@Autowired
+	private TokenService tokenService;
 
+	@Autowired
+	private PerfilRepository perfilRepository;
+
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+
+	private String token;
+	
+	@BeforeEach
+	public void gerarToken() {
+		Usuario logado = new Usuario("admin", "admin", "admin");
+		Perfil admin = perfilRepository.findById(1l).get();
+		logado.adicionarPerfil(admin);
+		usuarioRepository.save(logado);
+		Authentication authentication = new UsernamePasswordAuthenticationToken(logado, logado.getLogin());
+		this.token = tokenService.gerarToken(authentication);		
+	}
+	
 	@Test
 	void naoDeveriaCadastrarLivroComDadosIncompletos() throws Exception {
 		String json = "{}";
-		mvc.perform(post("/livros").contentType(MediaType.APPLICATION_JSON).content(json)).andExpect(status().isBadRequest());
+		mvc.perform(post("/livros").contentType(MediaType.APPLICATION_JSON).content(json).header("Authorization",
+				"Bearer " + token))
+				.andExpect(status().isBadRequest());
 	}
 
 	@Test
@@ -44,8 +75,8 @@ public class LivroControllerTest {
 		String jsonAutorRetornado = "{\"nome\":\"Robert C. Martin\"," + "\"email\":\"unclebob@gmail.com\","
 				+ "\"dataNascimento\":\"1952-12-5\"," + "\"miniCurriculo\":\"clean code...\"}";
 
-		MvcResult resultado = mvc
-				.perform(post("/autores").contentType(MediaType.APPLICATION_JSON).content(jsonAutor))
+		MvcResult resultado = mvc.perform(post("/autores").contentType(MediaType.APPLICATION_JSON).content(jsonAutor).header("Authorization",
+				"Bearer " + token))
 				.andExpect(status().isCreated()).andExpect(header().exists("Location"))
 				.andExpect(content().json(jsonAutorRetornado)).andReturn();
 
@@ -56,8 +87,10 @@ public class LivroControllerTest {
 
 		String jsonLivroRetornado = "{\"titulo\":\"Código limpo: Habilidades práticas do Agile Software\",\"dataLancamento\":\"2009-9-8\",\"numeroDePaginas\":524,\"autor_id\":"
 				+ id + "}";
-		
-		mvc.perform(post("/livros").contentType(MediaType.APPLICATION_JSON).content(jsonLivro)).andExpect(status().isCreated()).andExpect(header().exists("Location"))
+
+		mvc.perform(post("/livros").contentType(MediaType.APPLICATION_JSON).content(jsonLivro).header("Authorization",
+				"Bearer " + token))
+				.andExpect(status().isCreated()).andExpect(header().exists("Location"))
 				.andExpect(content().json(jsonLivroRetornado));
 	}
 }
